6293088016230b07b755f36ec0b44653fba9f073
Shawn Levin <shawnlevin22@gmail.com>
2018-02-12 12:53:03 -0800

    initial client setup complete

---
+++ client/src/components/api_test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ApiTest extends Component {
+    render() {
+        return (
+            <div>
+                <h1>API testing page</h1>
+
+            </div>
+
+
+        )
+    }
+}
+
+export default ApiTest;
---
+++ client/src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+
+            <div>
+                <h1>This is home</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, aliquid. Consectetur magnam in aliquid, eaque rerum illum assumenda laboriosam porro nostrum! Veritatis, aspernatur provident repellendus debitis rerum voluptatem ullam iste commodi veniam laboriosam deserunt obcaecati suscipit qui, fuga sit accusamus nihil esse vitae? Necessitatibus eligendi maiores, ipsam ea iusto ipsa?</p>
+            </div>
+
+
+        )
+    }
+}
+
+export default Home;
