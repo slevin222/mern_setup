d73e2d63e991c05532e749063004743184dfd7df
Shawn Levin <shawnlevin22@gmail.com>
2018-02-12 16:05:28 -0800

    api setup completed

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const PORT = process.env.port || 9000;
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('<h1>App is working</h1>');
+    res.send('<h1>App is working! Now with Nodemon!</h1>');
 });
 
 app.get('/test-route', (req, res) => {
     res.send('<h1>This is a test route!</h1>');
+});
+
+app.get('/api/get-user', (req, res) => {
+    res.send({ username: 'Bob1234', email: 'bob@email.com', name: 'Bob' });
+});
+
+app.get('/api/get-articles', (req, res) => {
+    res.send(['article-1', 'article-2', 'article-3']);
 });
 
 app.listen(PORT, () => {
